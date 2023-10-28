79ce286a357e827e3380b9bed2beda5ca7050a70
darekgrados <dariuszgrados@gmail.com>
2023-10-28 09:22:30 +0200

    db context for customer controller

--- LibApp/Controllers/CustomersController.cs
+++ LibApp/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
-﻿using LibApp.Models;
+﻿using LibApp.Data;
+using LibApp.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,6 +7,12 @@ namespace LibApp.Controllers
 {
     public class CustomersController : Controller
     {
+        // DbContext will be polled through Dependency Injection
+        public CustomersController(ApplicationDbContext dbContext) 
+        {
+            _context = dbContext;
+        }
+
         // GET: CustomersController
         public ViewResult Index()
         {
@@ -34,6 +41,8 @@ namespace LibApp.Controllers
                 new Customer { Id = 2, Name = "Monika Nowak" }
             };
         }
+
+        private ApplicationDbContext _context;
     }
       
 }
